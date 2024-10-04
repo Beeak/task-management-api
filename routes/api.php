fe9cbc07299f82230d43c3b008f22ef15b2f0ae3
Gen Vikat <gen@veebisepad.ee>
2024-10-04 10:57:20 +0300

    refactored login and register endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskUserController;
 use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Session\Middleware\StartSession;
+
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])->middleware([StartSession::class, AddQueuedCookiesToResponse::class,]);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
